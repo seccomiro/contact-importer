dd63e2c2dfe1fda371fa2555d450149e3c50ca94
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-04-29 01:40:51 -0300

    Add franchise fetching to CreditCard

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,3 +1,9 @@
 class CreditCard < ApplicationRecord
+  include CreditCardValidable
+
+  validates :contact, presence: true
+  validates :number, presence: true
+  validates :franchise, presence: true
+
   belongs_to :contact
 end
--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :contact do
-    name { "MyString" }
-    email { "MyString" }
-    birthdate { "MyString" }
-    phone { "MyString" }
-    address { "MyString" }
-    credit_card { "MyString" }
-    franchise { "MyString" }
+    name { 'MyString' }
+    email { 'MyString' }
+    birthdate { DateTime.now }
+    phone { 'MyString' }
+    address { 'MyString' }
+    user
   end
 end
