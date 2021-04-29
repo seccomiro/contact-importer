ac50fa83f4f8a263f5efcd340e24c745f2a5973c
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-04-29 15:28:41 -0300

    Add valid examples do seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,10 @@ user = User.create(email: 'user@user.com', password: '321321321', password_confi
     name: Faker::FunnyName.unique.two_word_name,
     email: Faker::Internet.unique.email,
     birthdate: Faker::Date.between(from: 100.years.ago, to: Date.today),
-    # TODO: Refactor to fit validations
-    phone: Faker::PhoneNumber.cell_phone,
-    address: Faker::Address.full_address,
+    phone: '(+57) 320-432-05-09',
+    address: Faker::Address.full_address
   )
   contact.create_credit_card(
-    # TODO: Refactor to fit validations
-    number: Faker::Finance.credit_card(:visa),
-    # TODO: Refactor to fit validations
-    # franchise: ['Visa', 'American Express', 'MasterCard'].sample
+    number: Faker::Finance.credit_card(:visa)
   )
 end
