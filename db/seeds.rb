1243b7cc06e4e4971118793433cdf02eb31833fd
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-04-28 21:18:59 -0300

    Add seeders for User, Contact and CreditCard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(email: 'user@user.com', password: '321321321', password_confirmation: '321321321')
+10.times do
+  contact = user.contacts.create(
+    name: Faker::FunnyName.unique.two_word_name,
+    email: Faker::Internet.unique.email,
+    birthdate: Faker::Date.between(from: 100.years.ago, to: Date.today),
+    # TODO: Refactor to fit validations
+    phone: Faker::PhoneNumber.cell_phone,
+    address: Faker::Address.full_address,
+  )
+  contact.create_credit_card(
+    # TODO: Refactor to fit validations
+    number: Faker::Finance.credit_card,
+    # TODO: Refactor to fit validations
+    franchise: ['Visa', 'American Express', 'MasterCard'].sample
+  )
+end
