04638ac2811619dcb3f9924c9fa2cfcaa31ea437
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-04-29 10:09:38 -0300

    Improve contact validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,11 @@
 class Contact < ApplicationRecord
   belongs_to :user
   has_one :credit_card, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :birthdate, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
+  validates :user, presence: true
 end
