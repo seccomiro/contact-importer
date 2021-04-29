bcad5f1d1438235c8a8454601465cb7b74aac1fa
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-04-29 14:08:48 -0300

    Add Contact validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,10 +2,12 @@ class Contact < ApplicationRecord
   belongs_to :user
   belongs_to :credit_card, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, format: { with: /[-\w]/i }
   validates :email, presence: true
   validates :birthdate, presence: true
-  validates :phone, presence: true
+  validates :phone, presence: true, format: {
+    with: /\(\+[0-9]{2}\) [0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2} [0-9]{2}|\(\+[0-9]{2}\) [0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}/
+  }
   validates :address, presence: true
   validates :user, presence: true
   validates :email, uniqueness: { scope: :user_id }
