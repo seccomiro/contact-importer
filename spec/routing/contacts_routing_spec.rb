646e30b78051696c4917b9fb99d3533b8aeca690
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-05-19 17:23:02 -0300

    Remove routing specs

--- spec/routing/contacts_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ContactsController, type: :routing do
-  describe 'routing' do
-    it 'routes to #index' do
-      expect(get: '/contacts').to route_to('contacts#index')
-    end
-  end
-end
