423bd7a250a2d88a16c3eb51a5454342ec1f2a27
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-05-19 17:38:28 -0300

    Fix bugs at wrongly filtered queries at controllers

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,6 @@ class ContactsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @contacts = Contact.includes(:credit_card).paginate(page: params[:page], per_page: 5)
+    @contacts = current_user.contacts.includes(:credit_card).paginate(page: params[:page], per_page: 5)
   end
 end
