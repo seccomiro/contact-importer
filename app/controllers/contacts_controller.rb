b75e7c089bb0b7c7336e50471b84e22205aca133
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-05-20 13:37:15 -0300

    Create Contact visualization feature spec

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,7 @@ class ContactsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @contacts = current_user.contacts.includes(:credit_card).paginate(page: params[:page], per_page: 5)
+    @contacts = current_user.contacts.includes(:credit_card)
+                            .paginate(page: params[:page], per_page: params[:per_page] || 5)
   end
 end
---
+++ spec/features/contact_visualization_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.feature 'Contact visualization', type: :feature do
+  context 'with an authenticated user' do
+    let(:user) { create(:user) }
+
+    before do
+      sign_in user
+      create_list(:contact, 3, user: user, credit_card: build(:credit_card))
+    end
+
+    scenario 'User sees the contact list' do
+      visit contacts_path
+
+      expect(page).to have_selector('.contacts')
+      expect(page).to have_css('.contacts th', count: 7)
+      expect(page).to have_css('.contacts tbody tr', count: 3)
+    end
+
+    context 'with only one page of contacts' do
+      scenario 'User does not see pagination' do
+        visit contacts_path
+
+        expect(page).not_to have_selector('.pagination')
+      end
+    end
+
+    context 'with more than one page of contacts' do
+      scenario 'User sees pagination' do
+        visit contacts_path(params: { per_page: 2 })
+
+        expect(page).to have_selector('.pagination')
+        expect(page).to have_selector('.previous_page')
+        expect(page).to have_selector('.next_page')
+        expect(page).to have_link('2', href: contacts_path(params: { page: 2, per_page: 2 }))
+
+        click_link(2)
+
+        expect(page).to have_link('1', href: contacts_path(params: { page: 1, per_page: 2 }))
+        expect(page).to have_selector('.current', text: '2')
+      end
+    end
+  end
+
+  context 'with a guest' do
+    scenario 'User is redirected to the sign in page' do
+      visit contacts_path
+
+      expect(page).to have_current_path(new_user_session_path)
+    end
+  end
+end
