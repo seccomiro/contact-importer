0132e251f20aa4c149b90ab3c90584ca225c8b52
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-05-24 16:28:10 -0300

    Implement ZeroBounceClient

--- app/services/zero_bounce_client.rb
+++ app/services/zero_bounce_client.rb
@@ -1,2 +1,25 @@
 class ZeroBounceClient
+  def initialize
+    @api_key = ENV['ZEROBOUNCE_API_KEY']
+  end
+
+  def fetch(emails)
+    uri = URI('https://bulkapi.zerobounce.net/v2/validatebatch')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+    req.body = {
+      api_key: @api_key,
+      email_batch: emails.map do |e|
+        {
+          'email_address' => e,
+          'ip_address' => nil
+        }
+      end
+    }
+    res = http.request(req)
+
+    JSON.parse(res.body)
+  end
 end
