4a724472e5c3a8c6ad7a6124a596816a701fd8ab
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-05-25 19:38:04 -0300

    Handle email whitelisted emails at ZeroBounceClient

--- app/services/zero_bounce_client.rb
+++ app/services/zero_bounce_client.rb
@@ -1,11 +1,14 @@
 class ZeroBounceClient
   def initialize
     @api_key = ENV['ZEROBOUNCE_API_KEY']
-    @whitelist = whitelist
+    @whitelist = ZeroBounceClient.whitelist
     @allow_only_whitelisted = ENV['ZEROBOUNCE_ALLOW_ONLY_WHITELISTED'] == 'true'
   end
 
   def fetch(emails)
+    whitelisted_emails = emails.filter { |email| whitelisted?(email) }
+    return not_whitelisted_results(emails) if whitelisted_emails.none?
+
     uri = URI('https://bulkapi.zerobounce.net/v2/validatebatch')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -13,27 +16,33 @@ class ZeroBounceClient
     req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
     req.body = {
       api_key: @api_key,
-      email_batch: emails.map do |e|
-        {
-          'email_address' => e,
-          'ip_address' => nil
-        }
-      end
+      email_batch: whitelisted_emails.map { |email| { 'email_address' => email, 'ip_address' => nil } }
     }.to_json
     res = http.request(req)
 
-    JSON.parse(res.body)
+    result = JSON.parse(res.body)
+    raise StandardError, result['errors'] if result['errors'].any? && result['email_batch'].any?
+
+    result['email_batch'] + not_whitelisted_results(emails)
   end
 
-  # private
-
   # This list is needed to avoid charging the real ZeroBounce account credits
-  def whitelist
+  def self.whitelist
     yaml = YAML.load_file(Rails.root.join('lib/assets/zero_bounce_whitelist.yml'))
     yaml['whitelist']
   end
 
   def whitelisted?(email)
     @whitelist.include?(email) || !@allow_only_whitelisted
+  end
+
+  private
+
+  def not_whitelisted_results(emails)
+    emails
+      .filter { |email| !whitelisted?(email) }
+      .map do |email|
+        { 'address' => email, 'status' => 'not_whitelisted' }
+      end
   end
 end
--- spec/services/zero_bounce_client_spec.rb
+++ spec/services/zero_bounce_client_spec.rb
@@ -6,9 +6,12 @@ describe ZeroBounceClient do
   let(:valid_email) { 'valid@email.com' }
   let(:invalid_email) { 'invalid@email.com' }
   let(:api_url) { 'https://bulkapi.zerobounce.net/v2/validatebatch' }
+  let(:api_key) { 'api_key' }
+  let(:whitelisted_email) { described_class.whitelist.first }
+  let(:not_whitelisted_email) { 'not_whitelisted@email.com' }
   let(:valid_email_request_body) do
     {
-      'api_key' => ENV['ZEROBOUNCE_API_KEY'],
+      'api_key' => api_key,
       'email_batch' => [
         { 'email_address' => valid_email, 'ip_address' => nil }
       ]
@@ -18,14 +21,84 @@ describe ZeroBounceClient do
     {
       'email_batch' => [
         { 'email_address' => valid_email, 'status' => 'valid' }
-      ]
+      ], 'errors' => []
     }
   end
   let(:empty_request_body) do
-    { 'api_key' => ENV['ZEROBOUNCE_API_KEY'], 'email_batch' => [] }
+    { 'api_key' => api_key, 'email_batch' => [] }
   end
   let(:empty_response_body) do
-    { 'email_batch' => [], 'errors' => [] }
+    { 'email_batch' => [], 'errors' => [
+      { 'error' => 'Error message', 'email_address' => 'all' }
+    ] }
+  end
+  let(:error_request_body) do
+    {
+      'api_key' => nil,
+      'email_batch' => [
+        { 'email_address' => valid_email, 'ip_address' => nil }
+      ]
+    }
+  end
+  let(:error_response_body) do
+    { 'email_batch' => [
+      { 'email_address' => valid_email, 'status' => 'valid' }
+    ], 'errors' => [
+      { 'error' => 'Error message', 'email_address' => 'all' }
+    ] }
+  end
+  let(:whitelisted_email_request_body) do
+    {
+      'api_key' => api_key,
+      'email_batch' => [
+        { 'email_address' => whitelisted_email, 'ip_address' => nil }
+      ]
+    }
+  end
+  let(:whitelisted_email_response_body) do
+    {
+      'email_batch' => [
+        { 'address' => whitelisted_email, 'status' => 'valid' }
+      ], 'errors' => []
+    }
+  end
+
+  let(:not_whitelisted_email_request_body_with_whitelist_disabled) do
+    {
+      'api_key' => api_key,
+      'email_batch' => [
+        { 'email_address' => not_whitelisted_email, 'ip_address' => nil }
+      ]
+    }
+  end
+  let(:not_whitelisted_email_resonse_body_with_whitelist_disabled) do
+    {
+      'email_batch' => [not_whitelisted_email_result_with_whitelist_disabled], 'errors' => []
+    }
+  end
+  let(:whitelisted_and_not_whitelisted_email_request_body_with_whitelist_disabled) do
+    {
+      'api_key' => api_key,
+      'email_batch' => [
+        { 'email_address' => whitelisted_email, 'ip_address' => nil },
+        { 'email_address' => not_whitelisted_email, 'ip_address' => nil },
+      ]
+    }
+  end
+  let(:whitelisted_and_not_whitelisted_email_resonse_body_with_whitelist_disabled) do
+    {
+      'email_batch' => [whitelisted_email_result, not_whitelisted_email_result_with_whitelist_disabled], 'errors' => []
+    }
+  end
+
+  let(:whitelisted_email_result) { { 'address' => whitelisted_email, 'status' => 'valid' } }
+  let(:not_whitelisted_email_result) { { 'address' => not_whitelisted_email, 'status' => 'not_whitelisted' } }
+  let(:not_whitelisted_email_result_with_whitelist_disabled) { { 'address' => not_whitelisted_email, 'status' => 'valid' } }
+
+  before do
+    allow(ENV).to receive(:[]).and_call_original
+    allow(ENV).to receive(:[]).with('ZEROBOUNCE_ALLOW_ONLY_WHITELISTED').and_return('false')
+    allow(ENV).to receive(:[]).with('ZEROBOUNCE_API_KEY').and_return(api_key)
   end
 
   describe '#fetch' do
@@ -36,6 +109,21 @@ describe ZeroBounceClient do
       stub_request(:post, api_url)
         .with(body: empty_request_body.to_json)
         .to_return(body: empty_response_body.to_json)
+      stub_request(:post, api_url)
+        .with(body: error_request_body.to_json)
+        .to_return(body: error_response_body.to_json)
+      stub_request(:post, api_url)
+        .with(body: error_request_body.to_json)
+        .to_return(body: error_response_body.to_json)
+      stub_request(:post, api_url)
+        .with(body: whitelisted_email_request_body.to_json)
+        .to_return(body: whitelisted_email_response_body.to_json)
+      stub_request(:post, api_url)
+        .with(body: not_whitelisted_email_request_body_with_whitelist_disabled.to_json)
+        .to_return(body: not_whitelisted_email_resonse_body_with_whitelist_disabled.to_json)
+      stub_request(:post, api_url)
+        .with(body: whitelisted_and_not_whitelisted_email_request_body_with_whitelist_disabled.to_json)
+        .to_return(body: whitelisted_and_not_whitelisted_email_resonse_body_with_whitelist_disabled.to_json)
     end
 
     it 'requests ZeroBounce API endpoint' do
@@ -51,33 +139,99 @@ describe ZeroBounceClient do
     context 'with a valid email' do
       subject { client.fetch([valid_email]) }
 
-      it { is_expected.to include(valid_email_response_body) }
+      it { is_expected.to match_array(valid_email_response_body['email_batch']) }
     end
 
     context 'without any email' do
       subject { client.fetch([]) }
 
-      it { is_expected.to include('email_batch' => []) }
-      it { is_expected.to include('errors') }
+      it { is_expected.to eq([]) }
+    end
+
+    context 'with an actual error' do
+      subject { client.fetch([valid_email]) }
+
+      before do
+        allow(ENV).to receive(:[]).with('ZEROBOUNCE_API_KEY').and_return(nil)
+      end
+
+      it 'raises an error' do
+        expect { subject }.to raise_error(StandardError)
+      end
+    end
+
+    context 'with email whitelisting enabled' do
+      before do
+        allow(ENV).to receive(:[]).with('ZEROBOUNCE_ALLOW_ONLY_WHITELISTED').and_return('true')
+      end
+
+      context 'with a whitelisted email' do
+        subject { client.fetch([whitelisted_email]) }
+
+        it { is_expected.to eq([whitelisted_email_result]) }
+      end
+
+      context 'with an email not whitelisted' do
+        subject { client.fetch([not_whitelisted_email]) }
+
+        it { is_expected.to eq([not_whitelisted_email_result]) }
+
+        it 'returns a list containing the not whitelisted email with status not whitelisted' do
+          expect(subject).to contain_exactly(not_whitelisted_email_result)
+        end
+      end
+
+      context 'with a whitelisted email and an email not whitelisted' do
+        subject { client.fetch([whitelisted_email, not_whitelisted_email]) }
+
+        it { expect(subject).to contain_exactly(not_whitelisted_email_result, whitelisted_email_result) }
+      end
+    end
+
+    context 'with email whitelisting disabled' do
+      before do
+        allow(ENV).to receive(:[]).with('ZEROBOUNCE_ALLOW_ONLY_WHITELISTED').and_return('false')
+      end
+
+      context 'with a whitelisted email' do
+        subject { client.fetch([whitelisted_email]) }
+
+        it { is_expected.to eq([whitelisted_email_result]) }
+      end
+
+      context 'with an email not whitelisted' do
+        subject { client.fetch([not_whitelisted_email]) }
+
+        it { is_expected.to eq([not_whitelisted_email_result_with_whitelist_disabled]) }
+
+        it 'returns a list containing the not whitelisted email with status not whitelisted' do
+          expect(subject).to contain_exactly(not_whitelisted_email_result_with_whitelist_disabled)
+        end
+      end
+
+      context 'with a whitelisted email and an email not whitelisted' do
+        subject { client.fetch([whitelisted_email, not_whitelisted_email]) }
+
+        it { expect(subject).to contain_exactly(not_whitelisted_email_result_with_whitelist_disabled, whitelisted_email_result) }
+      end
     end
   end
 
-  describe '#whitelist' do
+  describe '.whitelist' do
     let(:filename) { Rails.root.join('lib/assets/zero_bounce_whitelist.yml') }
     let(:whitelist_from_file) { YAML.load_file(filename)['whitelist'] }
 
     it 'returns the correct ZeroBounce whitelist from the YAML file' do
-      expect(client.whitelist).to match_array(whitelist_from_file)
+      expect(described_class.whitelist).to match_array(whitelist_from_file)
     end
   end
 
   describe '#whitelisted?' do
-    let(:whitelisted_email) { client.whitelist.first }
+    let(:whitelisted_email) { described_class.whitelist.first }
     let(:not_whitelisted_email) { 'a_not_whitelisted@email.com' }
 
     context 'with email whitelisting enabled' do
       before do
-        allow(ENV).to receive(:[]).and_call_original
         allow(ENV).to receive(:[]).with('ZEROBOUNCE_ALLOW_ONLY_WHITELISTED').and_return('true')
       end
 
@@ -96,7 +250,6 @@ describe ZeroBounceClient do
 
     context 'with email whitelisting disabled' do
       before do
-        allow(ENV).to receive(:[]).and_call_original
         allow(ENV).to receive(:[]).with('ZEROBOUNCE_ALLOW_ONLY_WHITELISTED').and_return('false')
       end
 
