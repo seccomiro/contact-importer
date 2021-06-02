e39d1d09b5304e0f8d83e2d0b3f66d947dbae7a5
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-06-02 20:55:21 -0300

    Configure Devise to fail with JSON

---
+++ app/lib/devise/failures/custom_failure_app.rb
@@ -0,0 +1,22 @@
+module Devise
+  module Failures
+    class CustomFailureApp < Devise::FailureApp
+      def respond
+        if request.format == :json
+          json_error_response
+        else
+          super
+        end
+      end
+
+      def json_error_response
+        self.status = 401
+        self.content_type = 'application/json'
+        self.response_body = {
+          status: :error,
+          message: 'You need to sign in or sign up before continuing'
+        }.to_json
+      end
+    end
+  end
+end
