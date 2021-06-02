586e73f1c39bc327a2635ce57b9969cf228f1426
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-06-02 20:55:50 -0300

    Update Api::V1::Contacts specs

--- spec/requests/api/v1/contacts_spec.rb
+++ spec/requests/api/v1/contacts_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Contacts', type: :request do
+  let(:json) { JSON.parse(response.body) }
+
   context 'with an authenticated user' do
     let(:user) { create(:user) }
     let(:auth_headers) do
@@ -9,9 +11,9 @@ RSpec.describe 'Api::V1::Contacts', type: :request do
         'Accept' => 'application/json'
       }
     end
-    let!(:contacts) { create_list(:contact, 5, credit_card: create(:credit_card), user: user) }
+    let!(:contact) { create(:contact, credit_card: create(:credit_card), user: user) }
 
-    describe 'GET /index' do
+    describe 'GET /api/v1/contacts' do
       before do
         get api_v1_contacts_path, headers: auth_headers
       end
@@ -24,36 +26,58 @@ RSpec.describe 'Api::V1::Contacts', type: :request do
         expect(response.content_type).to include('application/json')
       end
 
-      it 'has a valid JSend response' do
-        json = JSON.parse(response.body)
-
+      it 'returns a valid JSend response' do
         expect(json).to include('status', 'data')
         expect(json['status']).to eq('success')
         expect(json['data']).to include('contacts')
       end
 
       it 'returns the contacts of the user' do
-        json = JSON.parse(response.body)
+        expect(json['data']['contacts'].size).to eq(user.contacts.count)
+      end
 
-        expect(json['data']['contacts'].size).to eq(contacts.size)
+      it 'returns contacts whith the correct structure and data' do
+        json_contact = json['data']['contacts'].first
+        expect(json_contact).to include(
+          {
+            'id' => contact.id,
+            'name' => contact.name,
+            'email' => contact.email,
+            'phone' => contact.phone,
+            'address' => contact.address,
+            'credit_card' => {
+              'number' => contact.credit_card.number,
+              'franchise' => contact.credit_card.franchise
+            }
+          }
+        )
       end
     end
   end
 
-  # TODO: Add more failing scenarios
-  # context 'with a guest' do
-  #   let(:guest_headers) do
-  #     {
-  #       'Accept' => 'application/json'
-  #     }
-  #   end
+  context 'with a guest' do
+    let(:guest_headers) { { 'Accept' => 'application/json' } }
 
-  #   before do
-  #     get api_v1_contacts_path, headers: guest_headers
-  #   end
+    shared_examples_for 'a user that is not authenticated' do
+      it 'returns an error' do
+        expect(response).to have_http_status(:unauthorized)
+      end
 
-  #   it 'returns an error' do
-  #     get api_v1_contacts_path
-  #   end
-  # end
+      it 'returns JSON' do
+        expect(response.content_type).to include('application/json')
+      end
+
+      it 'returns a valid JSON authentication error response' do
+        expect(json).to include('status', 'message')
+        expect(json['status']).to eq('error')
+        expect(json['message']).to eq('You need to sign in or sign up before continuing')
+      end
+    end
+
+    describe 'GET /api/v1/contacts' do
+      before { get api_v1_contacts_path, headers: guest_headers }
+
+      it_behaves_like 'a user that is not authenticated'
+    end
+  end
 end
