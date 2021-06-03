61c0aa5ebdb0f3811f52c80cd11cf9ac568a9760
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-06-03 15:50:26 -0300

    Respond with better structured JSON error response

--- app/controllers/api/v1/concerns/exception_handler.rb
+++ app/controllers/api/v1/concerns/exception_handler.rb
@@ -3,11 +3,22 @@ module Api::V1::Concerns::ExceptionHandler
 
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      json_fail_response(e.message, :not_found)
+      json_fail_response(error_data(e), :not_found)
     end
 
     rescue_from ActiveRecord::RecordInvalid do |e|
-      json_fail_response(e.message, :unprocessable_entity)
+      json_fail_response(error_data(e), :unprocessable_entity)
     end
+
+    # TODO: Check how to rescue from a 400 Bad Request
+    # rescue_from ActionController::BadRequest do |e|
+    #   json_error_response(e.message, :bad_request)
+    # end
+  end
+
+  private
+
+  def error_data(error)
+    error.respond_to?(:record) ? error.record.errors.messages : error.message
   end
 end
--- app/controllers/api/v1/concerns/response.rb
+++ app/controllers/api/v1/concerns/response.rb
@@ -1,6 +1,11 @@
 module Api::V1::Concerns::Response
   def json_fail_response(data = nil, status = :bad_request)
     @data = data
-    render 'api/v1/fail', status: status
+    render 'api/v1/errors/fail', status: status
+  end
+
+  def json_error_response(message = nil, status = :bad_request)
+    @message = message
+    render 'api/v1/errors/error', status: status
   end
 end
