3211464ab83f8e42726d4d5c3bfb46178b7ed824
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-06-03 17:03:17 -0300

    Make confirmation mimic password at User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@user.com" }
     password { '321321321' }
-    password_confirmation { '321321321' }
+    password_confirmation { password }
   end
 end
